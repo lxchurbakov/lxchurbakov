65251796c828557ba59776024dac7e610d2c40a6
lxchurbakov <lxchurbakov@gmail.com>
2024-01-19 10:54:24 +0300

    Update readme template

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 fs.writeFileSync('./readme.md', `
-    # LXCH
+# Hey everyone, my name is Alexander
+
+And I am a Fullstack Javascript Engineer with more than 8 years of experience and wannbe indiehacker.
 `, (err) => {
     if (err) {
         process.exit(1);
